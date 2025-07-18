d55653795fe0b75af0f13ba7a62dd99a08c2c6b0
steve-jones-sp <steve.jones@sailpoint.com>
2025-07-17 18:14:43 -0600

    had old python 3.7 support, replaced with 3.12

--- setup.py
+++ setup.py
@@ -51,15 +51,15 @@ setup(
         "Topic :: System :: Systems Administration",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Operating System :: OS Independent",
     ],
     keywords="sigma sumo-logic cse siem detection rules security cybersecurity",
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=read_requirements(),
     extras_require={
         "dev": [
